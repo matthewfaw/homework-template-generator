36b3723f73807ec97e0218c8c7b974810e5aae98
Matthew Faw <matthewfaw@gmail.com>
2020-07-29 15:19:39 -0500

    Refactors code to be python3 compatible. Adds ability to generate base paper or homework

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
-# homework-template-generator
+# Homework and Paper Template Generator
 
-A script to generate LaTeX files for homework. I recommend doing the following:
-- Populate `courses.py` with the courses you're taking
+A script to generate LaTeX files for homework and papers. I recommend doing the following:
+- If you want to use the course feature, populate `courses.py` with the courses you're taking
 - In your `.zshrc` file, add the following two lines:
 ```
 export LATEX_GENERATOR_SCRIPT_PATH=/full/path/to/create_hw.py
 alias genlatex="python $LATEX_GENERATOR_SCRIPT_PATH"
 ```
-- Then, if you want to see which courses are listed, run `genlatex list <?Semester>` to list the courses (optionally for only a selected semester)
+- Then, if you want to see which courses are listed, run `genlatex --list [--semester "<Semester>"]` to list the courses (optionally for only a selected semester)
 - When you're ready to create a new homework, create a directory for this homework, and from this dir, run:
 ```
-genlatex "<COURSE_ID>" <Problem Set Number> <Due Date and Time> "<Collaborators>"
+genlatex --generate-hw --course-id "<COURSE_ID>" --pset-number <Problem Set Number> --due-date <Due Date and Time> --collaborators "<?Collaborators>"
+```
+- When you want to create a new paper, create a directory for this paper, and
+  from this dir, run:
+```
+genlatex --generate-paper --title "<PAPER TITLE>"
 ```
--- create_hw.py
+++ create_hw.py
@@ -1,31 +1,40 @@
-import sys, re, os
+import sys, re, os, argparse
 from templates.courses import courses
 
 SCRIPT_DIRNAME=os.path.dirname(os.path.realpath(__file__))
 
-F_LIST = "list"
-F_GENERATE = "generate"
-function = ""
-TEX_TEMPLATE_FILE = "hw_template_2.tex"
-
-if len(sys.argv) >= 2 and sys.argv[1] == F_LIST:
-    function = F_LIST
-elif len(sys.argv) == 5:
-    function = F_GENERATE
-else:
-    print "Required arguments are either:"
-    print "a) COURSE_ID PSET_NUMBER DUE_DATE COLLABORATORS (to generate LaTeX template)"
-    print "or"
-    print "b) list [SEMESTER] (to list all valid COURSE_IDs)"
-    sys.exit(1)
-
-def copy_file(filename):
-    with open("%s/templates/%s" % (SCRIPT_DIRNAME, filename),"r") as template:
-        with open("%s" % (filename),"w+") as out:
+def copy_file(filename, new_name=""):
+    with open("{}/templates/{}".format(SCRIPT_DIRNAME, filename),"r") as template:
+        alt_name = new_name+os.path.splitext(filename)[1]
+        out_name = alt_name if new_name else filename
+        with open(out_name,"w+") as out:
             for line in template:
                 out.write(line)
 
-def create_template(semester, course_id, pset_number, due_date, collaborators, course_info):
+def list_course_ids(_semester):
+    print("Listing all valid COURSE_IDs for semester {}:".format(_semester))
+    for semester, info in courses["SEMESTER"].iteritems():
+        if _semester == semester or _semester == "ALL":
+            print("======")
+            print(semester)
+            print("======")
+            for course_id, course_info in info["COURSE_ID"].iteritems():
+                print("{}: {}, {}".format(course_id,\
+                        course_info["COURSE_NUMBER"],\
+                        course_info["COURSE_NAME"]))
+
+def create_template(template_file, output_file_name, replace_dict):
+    with open(output_file_name+".latexmain",'a'):
+        # Create the main file indicator
+        os.utime(output_file_name+".latexmain", None)
+    with open("{}/templates/{}".format(SCRIPT_DIRNAME,template_file),"r") as template:
+        with open(output_file_name + ".tex","w+") as out:
+            for line in template:
+                for key, val in replace_dict.items():
+                    line = line.replace(key, val)
+                out.write(line)
+
+def create_hw_template(template_file, semester, course_id, pset_number, due_date, collaborators, course_info):
     course_number = course_info["COURSE_NUMBER"]
     course_name = course_info["COURSE_NAME"]
     instr_names = course_info["INSTRUCTOR_NAMES"]
@@ -34,65 +43,82 @@ def create_template(semester, course_id, pset_number, due_date, collaborators, c
         print("Missing course name or instructor name in course_info! Cannot continue")
         sys.exit(1)
     else:
-        print "Determined course name: %s and instructor(s): %s" % \
-                (course_name, instr_names)
+        print("Determined course name: {} and instructor(s): {}".format(course_name, instr_names))
     multiple_instructors = instr_names.count(',') > 0
-    output_file_name = "ps%s_%s_%s" % (pset_number, \
+    output_file_name = "ps{}_{}_{}".format(pset_number, \
             course_number.replace(" ","_"), \
             re.sub(r'[\s,]+','_',semester))
-    with open(output_file_name+".latexmain",'a'):
-        # Create the main file indicator
-        os.utime(output_file_name+".latexmain", None)
-    with open("%s/templates/%s" % (SCRIPT_DIRNAME,TEX_TEMPLATE_FILE),"r") as template:
-        with open(output_file_name + ".tex","w+") as out:
-            for line in template:
-                line = line.replace("<ASSIGNMENT_NUMBER>",pset_number)
-                line = line.replace("<SEMESTER>",semester)
-                line = line.replace("<COURSE_ID>",course_number)
-                line = line.replace("<INSTRUCTOR_PLURAL>","s" if multiple_instructors else "")
-                line = line.replace("<INSTRUCTOR_NAMES>",instr_names)
-                line = line.replace("<NAME>", "Anonymous" if anonymous \
-                        else "Matthew Faw")
-                line = line.replace("<DUE_DATE>",due_date)
-                line = line.replace("<COLLABORATORS>", "Collaborators (in "
+
+    create_template(template_file=template_file,
+            output_file_name=output_file_name,
+            replace_dict={
+                "<ASSIGNMENT_NUMBER>": pset_number,
+                "<SEMESTER>": semester,
+                "<COURSE_ID>": course_number,
+                "<INSTRUCTOR_PLURAL>": "s" if multiple_instructors else "",
+                "<INSTRUCTOR_NAMES>": instr_names,
+                "<NAME>": "Anonymous" if anonymous else "Matthew Faw",
+                "<DUE_DATE>": due_date,
+                "<COLLABORATORS>": "Collaborators (in "
                         +"accordance with the course collaboration policy): "
-                        +"{}\\\\".format(collaborators) if collaborators != "" else "")
-                out.write(line)
+                        +"{}\\\\".format(collaborators) if collaborators != "" else ""
+                })
 
-def list_course_ids(_semester):
-    print "Listing all valid COURSE_IDs for semester %s:" % (_semester)
-    for semester, info in courses["SEMESTER"].iteritems():
-        if _semester == semester or _semester == "ALL":
-            print "======"
-            print semester
-            print "======"
-            for course_id, course_info in info["COURSE_ID"].iteritems():
-                print "%s: %s, %s" % (course_id,\
-                        course_info["COURSE_NUMBER"],\
-                        course_info["COURSE_NAME"])
+def create_paper_template(template_file, title):
+    create_template(template_file=template_file,
+            output_file_name=title,
+            replace_dict={"<TITLE>": title})
 
-if function == F_LIST:
-    semester = "ALL"
-    if len(sys.argv) > 2:
-        semester = sys.argv[2]
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Create a template")
+    parser.add_argument("--list", action='store_true')
+    parser.add_argument("--generate-hw", action='store_true')
+    parser.add_argument("--generate-paper", action='store_true')
+    parser.add_argument("--semester", type=str, default="ALL", required=False,
+            help="The semester to list courses. Only used when listing courses")
+    parser.add_argument("--course-id", type=str, default="", required=False,
+            help="The course id in courses.py")
+    parser.add_argument("--pset-number", type=str, default="", required=False,
+            help="The problem set number")
+    parser.add_argument("--due-date", type=str, default="", required=False,
+            help="The problem set due date")
+    parser.add_argument("--collaborators", type=str, default="", required=False,
+            help="Comma separated list of collaborators")
+    parser.add_argument("--title", type=str, default="", required=False,
+            help="Title for paper")
+    args = parser.parse_args()
 
-    list_course_ids(semester)
-elif function == F_GENERATE:
-    target_course_id=sys.argv[1]
-    pset_number=sys.argv[2]
-    due_date=sys.argv[3]
-    collaborators=sys.argv[4]
+    if args.list:
+        list_course_ids(args.semester)
+    elif args.generate_hw:
+        made_template = False
+        for semester in courses["SEMESTER"].iterkeys():
+            for course_id, course_info in courses["SEMESTER"][semester]["COURSE_ID"].iteritems():
+                if args.course_id == course_id:
+                    print("Creating template for {} {}".format(semester,course_id))
+                    create_hw_template(template_file="hw_template_2.tex",
+                            semester=semester,
+                            course_id=course_id,
+                            pset_number=args.pset_number,
+                            due_date=args.due_date,
+                            collaborators=args.collaborators,
+                            course_info=course_info)
+                    copy_file("mfhw.sty")
+                    copy_file("p1.tex")
+                    made_template = True
 
-    made_template = False
-    for semester in courses["SEMESTER"].iterkeys():
-        for course_id, course_info in courses["SEMESTER"][semester]["COURSE_ID"].iteritems():
-            if target_course_id == course_id:
-                print("Creating template for %s %s" % (semester,course_id))
-                create_template(semester, course_id, pset_number, due_date, collaborators, course_info)
-                copy_file("mfhw.sty")
-                copy_file("p1.tex")
-                made_template = True
-
-    if not made_template:
-        print("Failed to find course_info for course id: %s"%(target_course_id))
+        if not made_template:
+            print("Failed to find course_info for course id: {}".format(args.course_id))
+            sys.exit(1)
+    elif args.generate_paper:
+        print("Creating paper titled {}".format(args.title))
+        create_paper_template(template_file="research-template.tex",
+                title=args.title)
+        copy_file("research-template.sty", new_name=args.title)
+        copy_file("abstract.tex")
+        copy_file("body.tex")
+        copy_file("research-template.bib", new_name=args.title)
+        copy_file("appendix.tex")
+    else:
+        print("No function specified -- cannot proceed")
         sys.exit(1)
